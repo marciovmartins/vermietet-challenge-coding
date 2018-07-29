41352ff6891fcf1c7339429018799b0364b4280f
Marcio Vinicius <marciovmartins@hotmail.com>
2018-07-29 19:14:16 -0300

    - added jacoco.

--- src/main/infrastructure/vermietet/challenge/coding/LocalEnvironment.java
+++ src/main/infrastructure/vermietet/challenge/coding/LocalEnvironment.java
@@ -3,7 +3,7 @@ package vermietet.challenge.coding;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LocalEnvironment implements Environment { // TODO: tests need to be implemented.
+public class LocalEnvironment implements Environment {
     @Override
     public String get(String key) {
         return System.getenv(key);
